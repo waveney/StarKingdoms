56c7ab460ea1ecd49ea82806c5ad75dcae83dfb1
waveney <richard@wavwebs.com>
2021-11-20 09:22:17 +0000

    Initial commit

---
+++ sk.php
@@ -0,0 +1,391 @@
+<?php
+
+/* Various common code across fest con tools */
+
+  include_once("skdb.php");
+  include_once("skfm.php");
+
+$BUTTON = 0;
+
+if (isset($_REQUEST{'Y'})) $YEAR = $_REQUEST{'Y'};
+if (isset($_REQUEST{'B'})) $BUTTON = ($_REQUEST{'B'}+1) % 4;
+
+if (isset($YEAR)) {
+  if (strlen($YEAR)>10) exit("Invalid Year");
+}
+
+$Access_Levels = ['','Participant','Observer','Player','GM','God','SysAdmin','Internal'];
+$Access_Type = array_flip($Access_Levels);
+
+// Perfname => [field to test, email address for,Capability name,budget,shortCode]
+$Months = ['','Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec'];
+
+date_default_timezone_set('GMT');
+
+function Set_User() {
+  global $db,$USER,$USERID,$AccessType,$YEAR,$CALYEAR;
+  if (isset($USER)) return;
+  $USER = array();
+  $USERID = 0;
+  if (isset($_COOKIE{'SKD'})) {
+    $biscuit = $_COOKIE{'SKD'};
+    $Cake = openssl_decrypt($biscuit,'aes-128-ctr','Quarterjack',0,'BrianMBispHarris');
+    $crumbs = explode(':',$Cake);
+    $USER{'Subtype'} = $crumbs[0];
+    $USER{'AccessLevel'} = $crumbs[1];
+    $USERID = $USER{'UserId'} = $crumbs[2];
+    if ($USERID) return;
+    $USER = array();
+    $USERID = 0;
+  }
+  if (isset($_COOKIE{'SKC2'})) {
+    $res=$db->query("SELECT * FROM People WHERE Yale='" . $_COOKIE{'SKC2'} . "'");
+    if ($res) {
+      $USER = $res->fetch_assoc();
+      $USERID = $USER['UserId'];
+      $db->query("UPDATE People SET LastAccess='" . time() . "' WHERE UserId=$USERID" );
+// Track suspicious things
+      if (($USER['LogUse']) && (file_exists("LogFiles"))) {
+        $logf = fopen("LogFiles/U$USERID.txt",'a+');
+        if ($logf) {
+          fwrite($logf,date('d/m H:j:s '));
+          fwrite($logf,$_SERVER['PHP_SELF']);
+          fwrite($logf,json_encode($_REQUEST));
+          if ($_COOKIE) fwrite($logf,json_encode($_COOKIE));
+          if ($_FILES) fwrite($logf,json_encode($_FILES));
+          fwrite($logf,"\n\n");
+        }
+      }
+    }
+  } 
+}
+
+function Access($level,$subtype=0,$thing=0) {
+  global $Access_Type,$USER,$USERID;
+  $want = $Access_Type[$level];
+  Set_User();
+  if (!isset($USER{'AccessLevel'})) return 0;
+  if ($USER{'AccessLevel'} < $want) return 0;
+  
+  if ($USER{'AccessLevel'} > $want+1) return 1;
+
+  switch  ($USER{'AccessLevel'}) {
+
+  case $Access_Type['Participant'] : 
+    if (!$subtype) return 1;
+    if ($USER['Subtype'] == 'Other' && $subtype == 'Act') {}
+    elseif ($USER{'Subtype'} != $subtype) return 0;
+    return $thing == $USERID;
+
+  case $Access_Type['Upload'] :
+  case $Access_Type['Staff'] :
+  case $Access_Type['Steward'] :
+    if (!$subtype) return $USER{'AccessLevel'} >= $want;
+    if (isset($USER[$subtype]) && $USER[$subtype]) return 1;
+    return 0; 
+
+
+  case $Access_Type['Committee'] :
+    if (!$subtype) return 1;
+    if (isset($USER[$subtype]) && $USER[$subtype]) return 1;
+    return 0;
+
+  case $Access_Type['SysAdmin'] : 
+    return 1;
+
+  case $Access_Type['Internal'] : 
+    return 1;
+
+  default:
+    return 0;
+  }
+}
+
+
+/*
+  If not in session 
+    If Yale then
+      Find User from Yale, start session
+      if not found - Login page
+    else 
+      Login page
+  endif
+  if AccessOK return
+  else isuf priv page
+*/
+
+function A_Check($level,$subtype=0,$thing=0) {
+  global $Access_Type,$USER,$USERID;
+  global $db;
+  Set_User();
+  if (!$USERID) {
+    include_once("Login.php");
+    Login();
+  }
+  if (Access($level,$subtype,$thing)) return;
+//echo "Failed checking...";
+//exit;
+  Error_Page("Insufficient Privilages");
+}
+
+function UserSetPref($pref,$val) {
+  global $USER,$USERID;
+  Set_User();
+  if (!$USERID) return; // No user
+  $Prefs = $USER['Prefs'];
+  if (!($NewPrefs = preg_replace("/$pref\:.*\n/","$pref:$val\n",$Prefs))) $NewPrefs = $Prefs .  "$pref:$val\n";
+  $USER['Prefs'] = $NewPrefs;
+  Put_User($USER);
+}
+
+function UserGetPref($pref) {
+  global $USER,$USERID;
+  if (!$USER || !isset($USER['Prefs'])) return 0;
+  if (preg_match("/$pref\:(.*)\n/",$USER['Prefs'],$rslt)) return trim($rslt[1]);
+  return 0;
+}
+
+
+function rand_string($len) {
+  $ans= '';
+  while($len--) $ans .= chr(rand(65,90));
+  return $ans;
+}
+
+
+function Get_User($who,&$newdata=0) {
+  global $db;
+  static $Save_User;
+
+  if (isset($Save_User[$who])) {
+    $ret = $Save_User[$who];
+    if ($newdata) $Save_User[$who] = $newdata;
+    return $ret;
+  }
+  $qry = "SELECT * FROM People WHERE Login='$who' OR Email='$who' OR UserId='$who'";
+  $res = $db->query($qry);
+
+  if (!$res || $res->num_rows == 0) return 0;
+  $data = $res->fetch_assoc();
+  $Save_User[$who] = ($newdata ? $newdata : $data);
+  return $data;
+}
+
+function Put_User(&$data,$Save_User=0) {
+  if (!$Save_User) $Save_User = Get_User($data['UserId'],$data);
+  Update_db('People',$Save_User,$data);
+}
+
+function Error_Page ($message) {
+  global $Access_Type,$USER,$USERID;
+  set_user();
+  if (isset($USER{'AccessLevel'})) { $type = $USER{'AccessLevel'}; } else { $type = 0; }
+//  var_dump($USER);
+//  echo "$type<p>";
+  switch ($type) {
+  case $Access_Type['Participant'] :
+    switch ( $USER{'Subtype'}) {
+    case 'Side' :
+    case 'Act' :
+    case 'Stall' :
+    case 'Sponsor' :
+    case 'Other' :
+    default:
+      include_once("index.php");
+      exit;
+    }
+
+  case $Access_Type['Committee'] :
+  case $Access_Type['Steward'] :
+  case $Access_Type['Staff'] :
+  case $Access_Type['Upload'] :
+//    $ErrorMessage = $message;
+//    var_dump($message);
+    include_once('int/Staff.php');  // Should be good
+    exit;                        // Just in case
+  case $Access_Type['SysAdmin'] :
+  case $Access_Type['Internal'] :
+    $ErrorMessage = "Something went very wrong... - $message";
+    include_once('Staff.php');  // Should be good
+    exit;                        // Just in case
+    
+  default:
+    include_once("index.php"); 
+  }
+}
+
+function Get_Game($y=0) {
+  global $db,$GAME,$GAMESYS;
+  if (!$y) $y=$GAMESYS['CurGame'];
+  $res = $db->query("SELECT * FROM Games WHERE Name='$y'");
+  if ($res) return $res->fetch_assoc();
+}
+
+$GAME = Get_Game();
+
+function First_Sent($stuff) {
+  $onefifty=substr($stuff,0,150);
+  return (preg_match('/^(.*?[.!?])\s/s',$onefifty,$m) ? $m[1] : $onefifty);
+}
+
+function munge_array(&$thing) {
+  if (isset($thing) && is_array($thing)) return $thing;
+  return [];
+}
+
+function Send_SysAdmin_Email($Subject,&$data=0) {
+  include_once("Email.php");
+  $dat = json_encode($data);
+  NewSendEmail(0,0,'richard@wavwebs.com',$Subject,$dat);  
+}
+
+$head_done = 0;
+$AdvancedHeaders = file_exists(dirname(__FILE__) . "/files/Newnavigation.php");
+
+function doextras($extras) {
+  global $FESTSYS;
+  $V=$FESTSYS['V'];
+  if ($extras) foreach ($extras as $e) {
+    $suffix=pathinfo($e,PATHINFO_EXTENSION);
+    if ($suffix == "js") {
+      echo "<script src=$e?V=$V></script>\n";
+    } else if ($suffix == 'jsdefer') {
+      $e = preg_replace('/jsdefer$/','js',$e);
+      echo "<script defer src=$e?V=$V></script>\n";
+    } else if ($suffix == "css") {
+      echo "<link href=$e?V=$V type=text/css rel=stylesheet>\n";
+    }
+  }
+}
+
+// If Banner is a simple image then treated as a basic banner image with title overlaid otherwise what is passed is used as is
+function dohead($title,$extras=[],$Banner='',$BannerOptions=' ') {
+  global $head_done,$FESTSYS,$CONF,$AdvancedHeaders;
+
+  if ($head_done) return;
+  $V=$FESTSYS['V'];
+  $pfx="";
+  if (isset($CONF['TitlePrefix'])) $pfx = $CONF['TitlePrefix'];
+  echo "<html><head>";
+  echo "<title>$pfx " . $FESTSYS['FestName'] . " | $title</title>\n";
+  if ($AdvancedHeaders) {
+    include_once("files/Newheader.php");
+  } else {
+    include_once("festfiles/header.php");
+  }  
+  if ($extras) doextras($extras);
+  echo "</head><body>\n";
+
+  if ($AdvancedHeaders) {
+    echo "<div class=contentlim>";  
+    include_once("files/Newnavigation.php");
+
+    if ($Banner) {
+      if ($Banner == 1) {
+        echo "<div class=WMFFBanner400><img src=" . $FESTSYS['DefaultPageBanner'] . " class=WMFFBannerDefault>";
+        echo "<div class=WMFFBannerText>$title</div>";
+        if (!strchr('T',$BannerOptions)) echo "<img src=/images/icons/torn-top.png class=TornTopEdge>";
+        echo "</div>";
+      } else if (preg_match('/^(https?:\/\/|\/?images\/)/',$Banner)) {
+        echo "<div class=WMFFBanner400><img src=$Banner class=WMFFBannerDefault>";
+        echo "<div class=WMFFBannerText>$title</div>";
+        if (!strstr($BannerOptions,'T')) echo "<img src=/images/icons/torn-top.png class=TornTopEdge>";
+        echo "</div>";
+      } else {
+        echo $Banner;
+      }
+    } else {
+      echo "<div class='NullBanner'></div>";  // Not shure this is needed
+    }
+  } else {
+    echo "<div class=contentlim>";  
+    include_once("skfiles/navigation.php");
+  }
+  echo "<div class=mainwrapper><div class=maincontent>";  
+  $head_done = 1;
+}
+
+//  No Banner 
+function doheadpart($title,$extras=[]) {
+  global $head_done,$GAMESYS,$CONF,$AdvancedHeaders,$GAME;
+  if ($head_done) return;
+  $V=$GAMESYS['V'];
+  $pfx="";
+  if (isset($CONF['TitlePrefix'])) $pfx = $CONF['TitlePrefix'];
+  echo "<html><head>";
+  echo "<title>$pfx " . $GAMES['Name'] . " | $title</title>\n";
+  if ($AdvancedHeaders) {
+    include_once("files/Newheader.php");
+  } else {
+    include_once("skfiles/header.php");
+  }  
+  
+  if ($extras) doextras($extras);
+  $head_done = 1;
+}
+
+// No Banner
+function dostaffhead($title,$extras=[]) {
+  global $head_done,$GAMESYS,$CONF,$AdvancedHeaders,$GAME;
+  if ($head_done) return;
+  $V=$GAMESYS['V'];
+  $pfx="";
+  if (isset($CONF['TitlePrefix'])) $pfx = $CONF['TitlePrefix'];
+
+  echo "<html><head>";
+  echo "<title>$pfx " . $GAME['Name'] . " | $title</title>\n";
+  if ($AdvancedHeaders && Feature('NewStyle') && ! UserGetPref('StaffOldFormat')) {
+    include_once("files/Newheader.php");
+    include_once("skgame.php");
+    if ($extras) doextras($extras);
+    echo "<meta http-equiv='cache-control' content=no-cache>";
+    echo "</head><body>\n";
+    include_once("files/Newnavigation.php");
+    echo "<div class=content>";  
+  } else {
+    include_once("skfiles/header.php");
+    include_once("skgame.php");
+    if ($extras) doextras($extras);
+    echo "<meta http-equiv='cache-control' content=no-cache>";
+    echo "</head><body>\n";
+
+    include_once("skfiles/navigation.php"); 
+
+    echo "<div class=content>";
+  }
+  $head_done = 1;
+}
+
+// No Banner
+function dominimalhead($title,$extras=[]) { 
+  global $head_done,$GAMESYS,$CONF,$AdvancedHeaders,$GAME;
+  $V=$GAMESYS['V'];
+  $pfx="";
+  if (isset($CONF['TitlePrefix'])) $pfx = $CONF['TitlePrefix'];
+  echo "<html><head>";
+  echo "<title>$pfx " . $GAME['Name'] . " | $title</title>\n";
+//  echo "<link href=files/Newstyle.css?V=$V type=text/css rel=stylesheet>";
+  echo "<script src=/js/jquery-3.2.1.min.js></script>";
+  if ($extras) doextras($extras);
+//  if ($GAMESYS['Analytics']) echo "<script>" . $GAMESYS['Analytics'] . "</script>";
+  echo "</head><body>\n";
+  $head_done = 2;
+}
+
+function dotail() {
+  global $head_done,$AdvancedHeaders;
+
+  echo "</div>";
+  if ($head_done == 1) {
+    if ($AdvancedHeaders) {
+      include_once("files/Newfooter.php"); // Not minimal head
+    } else {
+      include_once("skfiles/footer.php"); // Not minimal head
+    }
+  }
+  echo "</body></html>\n";
+  exit;
+}
+ 
+
+?>
---
+++ skdb.php
@@ -0,0 +1,322 @@
+<?php
+
+// If table's index is 'id' it does not need to be listed here
+$TableIndexes = array(  'Sides'=>'SideId', 'SideYear'=>'syId', 'FestUsers'=>'UserId', 'Venues'=>'VenueId', 'Events'=>'EventId', 
+                        'Bugs'=>'BugId', 'BigEvent'=>'BigEid', 'DanceTypes'=>'TypeId', 
+                        'Directory'=>'DirId', 'Documents'=>'DocId', 'EventTypes'=>'ETypeNo',
+                        'MusicTypes'=>'TypeId','TimeLine'=>'TLid', 'BandMembers'=>'BandMemId', 'ActYear'=>'ActId',
+                        'TradeLocs'=>'TLocId','Trade'=>'Tid','TradeYear'=>'TYid','VolYear'=>'VYid'
+                        );
+
+function db_open () {
+  global $db,$CONF;
+  if (@ $CONF = parse_ini_file("Configuration.ini")) {
+    @ $db = new mysqli($CONF['host'],$CONF['user'],$CONF['passwd'],$CONF['dbase']);
+  } else {
+    @ $db = new mysqli('localhost','sk','1L-D7aiwg1_jHggTPoiHTryuR52fpFAnz','sk');
+    $CONF = ['dbase'=>'sk'];
+  }
+  if (!$db || $db->connect_error ) die ('Could not connect: ' .  $db->connect_error);
+}
+
+db_open();
+
+function Logg($what) {
+  global $db,$USERID;
+  $qry = "INSERT INTO LogFile SET Who='$USERID', changed='" . date('d/m/y H:i:s') . "', What='" . addslashes($what) . "'";
+  $db->query($qry);
+}
+
+function table_fields($table) {
+  global $db,$CONF;
+  static $tables = array();
+  if (isset($tables[$table])) return $tables[$table];
+
+  $qry = "SELECT Column_Name, Data_type FROM information_schema.columns WHERE table_schema='" . $CONF['dbase'] ."' AND table_name='" . $table . "'";
+  $Flds = $db->query($qry);
+  while ($Field = $Flds->fetch_array()) {
+    $tables[$table][$Field['Column_Name']] = $Field['Data_type'];
+  }
+  return $tables[$table];
+}
+
+
+
+function Update_db($table,&$old,&$new,$proced=1) {
+  global $db;
+  global $TableIndexes;
+  global $UpdateLog;
+
+  $Flds = table_fields($table);
+  $indxname = (isset($TableIndexes[$table])?$TableIndexes[$table]:'id');
+  $newrec = "UPDATE $table SET ";
+  $fcnt = 0;
+
+//var_dump( $Flds);
+//echo "<p>$newrec<p>";
+
+  foreach ($Flds as $fname=>$ftype) {
+    if ($indxname == $fname) { // Skip
+    } elseif (isset($new[$fname])) {
+      if ($ftype == 'text') {
+        $dbform = addslashes($new[$fname]);
+      } elseif ($ftype == 'tinyint' || $ftype == 'smallint') {
+        $dbform = 0;
+        if ($new[$fname]) {
+          if ((string)(int)$new[$fname] = $new[$fname]) { $dbform = $new[$fname]; } else { $dbform = 1; };
+        }
+      } else {
+        $dbform = $new[$fname];
+      }
+
+//echo "$fname " . $old[$fname] . " $dbform<br>";
+      if (!isset($old[$fname]) || $dbform != $old[$fname]) {
+        $old[$fname] = $dbform;
+        if ($fcnt++ > 0) { $newrec .= " , "; }
+        $newrec .= " $fname=" . '"' . $dbform . '"';
+      }
+    } else {
+      if ($ftype == 'tinyint' || $ftype == 'smallint' ) {
+//      if ($fname == 'InUse') debug_print_backtrace();
+        if ($old[$fname]) {
+          $old[$fname] = 0;
+            if ($fcnt++ > 0) { $newrec .= " , "; }
+          $newrec .= " $fname=0";
+        }
+      } 
+    }
+  }
+
+//echo "$fcnt<p>";
+  if ($proced && $fcnt) {
+    $newrec .= " WHERE $indxname=" . $old[$indxname];
+//var_dump($newrec);
+    $update = $db->query($newrec);
+    $UpdateLog .= $newrec . "\n";
+    if ($update) {
+//      echo "<h2>$table Updated - $newrec</h2>\n";
+//      echo "<h2>$table Updated</h2>\n";
+    } else {
+      echo "<h2 class=ERR>An error occoured: ((($newrec))) " . $db->error . "</h2>";
+    }
+    return $update;
+  }
+}
+
+function Update_db_post($table, &$data, $proced=1) { 
+  return Update_db($table,$data,$_POST,$proced);
+}
+
+function Insert_db($table, &$from, &$data=0, $proced=1) {
+  global $db;
+  global $TableIndexes;
+  global $UpdateLog;
+  $newrec = "INSERT INTO $table SET ";
+  $fcnt = 0;
+  $Flds = table_fields($table);
+  $indxname = (isset($TableIndexes[$table])?$TableIndexes[$table]:'id');
+//echo "Fields: "; var_dump($from);
+  foreach ($Flds as $fname=>$ftype) {
+    if (isset($from[$fname]) && $from[$fname] != '' && $indxname!=$fname ) { 
+      if ($fcnt++ > 0) { $newrec .= " , "; }
+      if ($ftype == 'text') {
+        $dbform = addslashes($from[$fname]);
+        if ($data) $data[$fname] = $dbform;
+        $newrec .= " $fname=" . '"' . $dbform . '"';
+      } elseif ($ftype == "tinyint" || $ftype == 'smallint') {
+        $dbform = 0;
+        if ($from{$fname}) {
+          if ((string)(int)$from[$fname] = $from[$fname]) { $dbform = $from[$fname]; } else { $dbform = 1; };
+        }
+        if ($data) $data[$fname] = $dbform;
+        $newrec .= " $fname=$dbform ";
+      } else {
+        if ($data) $data[$fname] = $from[$fname];
+        $newrec .= " $fname=$from[$fname] ";
+      }
+    }
+  }
+//var_dump($newrec);
+  if ($proced) {
+    $insert = $db->query($newrec);
+    if ($insert) {
+      $UpdateLog .= $newrec . "\n";
+      $snum = $db->insert_id;
+//      echo "<h2>$table New entry - $newrec - $snum</h2>";
+//      echo "<h2>$table New entry added</h2>";
+      if ($data) $data[$indxname]=$snum;
+      $from[$indxname]=$snum;
+      return $snum;
+    } else {
+      echo "<h2 class=ERR>An error occoured: ((($newrec))) " . $db->error . "</h2>";
+    }
+  }
+  return 0;
+}
+
+function Insert_db_post($table,&$data,$proced=1) {
+  $data['Dummy'] = 1;
+  return Insert_db($table,$_POST,$data,$proced);  
+}
+
+function db_delete($table,$entry) {
+  global $db,$TableIndexes;
+  $indxname = (isset($TableIndexes[$table])?$TableIndexes[$table]:'id');
+//echo "DELETE FROM $table WHERE $indxname='$entry'<p>";
+  return $db->query("DELETE FROM $table WHERE $indxname='$entry'");
+}
+
+function db_delete_cond($table,$cond) {
+  global $db;
+  return $db->query("DELETE FROM $table WHERE $cond");
+}
+
+function db_update($table,$what,$where) {
+  global $db;
+  return $db->query("UPDATE $table SET $what WHERE $where");
+}
+
+function db_get($table,$cond) {
+  global $db;
+  $res = $db->query("SELECT * FROM $table WHERE $cond");
+  if ($res) return $res->fetch_assoc();
+  return 0;
+}
+
+// Read YEARDATA Data - this is NOT year specific - Get fest name, short name, version everything else is for future
+
+function Get_GAMESYS() {
+  global $db,$GAMESYS;
+  $res = $db->query("SELECT * FROM MasterData");
+  if ($res) return $res->fetch_assoc();
+}
+
+$GAMESYS = Get_GAMESYS();
+$CALYEAR = gmdate('Y');
+$GAME = $GAMESYS['CurGame'];  //$GAME can be overridden
+include_once("Version.php");
+$GAMESYS['V'] = $CALYEAR . ".$VERSION";
+
+function Feature($Name,$default='') {  // Return value of feature if set from FESTSYS
+  static $Features;
+  global $GAMESYS;
+  if (!$Features) {
+    $Features = [];
+    foreach (explode("\n",$GAMESYS['Features']) as $i=>$feat) {
+      $Dat = explode(":",$feat,4);
+      if ($Dat[0] && isset($Dat[1])) {
+        $Features[$Dat[0]] = trim($Dat[1]);
+      } elseif ($Dat[0] && isset($Dat[4])) {
+        $Features[$Dat[0]] = trim($Dat[4]);
+      }
+    }
+  }
+  if (isset($Features[$Name])) return $Features[$Name];
+  return $default;
+}
+
+function GameFeature($Name,$default='') {  // Return value of feature if set from FESTSYS
+  static $Features;
+  global $YEARDATA;
+  if (!$Features) {
+    $Features = [];
+    foreach (explode("\n",$GAMEDATA['GameFeatures']) as $i=>$feat) {
+      $Dat = explode(":",$feat,4);
+      if ($Dat[0] && isset($Dat[1])) {
+        $Features[$Dat[0]] = trim($Dat[1]);
+      } elseif ($Dat[0] && isset($Dat[4])) {
+        $Features[$Dat[0]] = trim($Dat[4]);
+      }
+    }
+  }
+  if (isset($Features[$Name])) return $Features[$Name];
+  return $default;
+}
+
+function Capability($Name,$default='') {  // Return value of Capability if set from GAMESYS
+  static $Capabilities;
+  global $GAMESYS;
+  if (!$Capabilities) {
+    $Capabilities = [];
+    foreach (explode("\n",$GAMESYS['Capabilities']) as $i=>$Cape) {
+      $Dat = explode(":",$Cape,3);
+      if ($Dat[0])$Capabilities[$Dat[0]] = trim($Dat[1]);
+    }
+  }
+  if (isset($Capabilities[$Name])) return $Capabilities[$Name];
+  return $default;
+}
+
+function set_ShowGame($last=0) { // Overrides default above if not set by a Y argument
+  global $GAME;
+}
+
+// Works for simple tables
+// Deletes = 0 none, 1=one, 2=many
+function UpdateMany($table,$Putfn,&$data,$Deletes=1,$Dateflds='',$Timeflds='',$Mstr='SN',$MstrNot='') {
+  global $TableIndexes;
+  include_once("DateTime.php");
+  $Flds = table_fields($table);
+  $DateFlds = explode(',',$Dateflds);
+  $TimeFlds = explode(',',$Timeflds);
+  $indxname = (isset($TableIndexes[$table])?$TableIndexes[$table]:'id');
+
+//var_dump($_POST);
+//return;
+  if (isset($_POST{'Update'})) {
+    if ($data) foreach($data as $t) {
+      $i = $t[$indxname];
+      if ($i) {
+        if (isset($_POST["$Mstr$i"]) && $_POST["$Mstr$i"] == $MstrNot) {
+          if ($Deletes) {
+//          echo "Would delete " . $t[$indxname] . "<br>";
+              db_delete($table,$t[$indxname]);
+            if ($Deletes == 1) return 1;
+          }
+          continue;
+        } else {
+          $recpres = 0;
+          foreach ($Flds as $fld=>$ftyp) {
+            if ($fld == $indxname) continue;
+            if (in_array($fld,$DateFlds)) {
+              $t[$fld] = Date_BestGuess($_POST["$fld$i"]);
+              $recpres = 1;
+            } else if (in_array($fld,$TimeFlds)) {
+              $t[$fld] = Time_BestGuess($_POST["$fld$i"]);
+              $recpres = 1;
+            } else if (isset($_POST["$fld$i"])) {
+              $t[$fld] = $_POST["$fld$i"];
+              $recpres = 1;
+            } else {
+              $t[$fld] = 0;
+            }
+          }
+//          var_dump($t);
+//          return;
+          if ($recpres) $Putfn($t);
+        }
+      }
+    }
+    if ($_POST[$Mstr . "0"] != $MstrNot) {
+      $t = array();
+      foreach ($Flds as $fld=>$ftyp) {
+        if ($fld == $indxname) continue;
+        if (isset($_POST[$fld . "0"])) {
+          if (in_array($fld,$DateFlds)) {
+            $t[$fld] = Date_BestGuess($_POST[$fld . "0"]);
+          } else if (in_array($fld,$TimeFlds)) {
+            $t[$fld] = Time_BestGuess($_POST[$fld . "0"]);
+          } else {
+            $t[$fld] = $_POST[$fld . "0"];
+          }
+        }
+      }
+      Insert_db($table,$t);
+    }
+    return 1;
+  } 
+}
+
+
+?>
+
