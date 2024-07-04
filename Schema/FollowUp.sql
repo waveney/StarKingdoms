caf2889177fca45f2399637559ee88d0a28275a3
waveney <richard@wavwebs.com>
2024-07-04 13:38:00 +0100

    db collation 2

--- Schema/FollowUp.sql
+++ Schema/FollowUp.sql
@@ -4,7 +4,7 @@ CREATE TABLE `FollowUp` (
   `Turn` int NOT NULL,
   `FactionId` int NOT NULL,
   `State` int NOT NULL,
-  `ActionNeeded` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ActionNeeded` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `Notes` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
--- Schema/ThingHistory.sql
+++ Schema/ThingHistory.sql
@@ -2,6 +2,6 @@ CREATE TABLE `ThingHistory` (
   `id` int NOT NULL AUTO_INCREMENT,
   `ThingId` int NOT NULL,
   `TurnNum` int NOT NULL,
-  `Text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Text` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
