5692a8729bd9614e8508a187a8009633c419aa58
waveney <richard@wavwebs.com>
2024-11-05 20:01:54 +0000

    What Can I see

--- WhatCanIC.php
+++ WhatCanIC.php
@@ -6,7 +6,7 @@
   include_once("ThingLib.php");
 
   A_Check('Player');
-  
+
   $GM = Access('GM');
 
   dostaffhead("What can I see");
@@ -23,22 +23,34 @@
   } else {
     if ( $FACTION['TurnState'] == 3) Player_Page();
   }
-  
+
   $File = "CouldC";
   if (isset($_REQUEST['C'])) $File = "Could" . $_REQUEST['C'];
 
   echo "<div class=floatright><h2>";
   if ($GAME['Turn'] <5 ) {
-    echo "Older Turns =&gt;";
-    if ($GAME['Turn']) for($turn=1; $turn <= $GAME['Turn']; $turn++) {
-      echo ", <a href=WhatCanIC.php?Y=$turn>$turn</a>";
+    echo "End of Older Turns =&gt;";
+    for($turn=1; $turn <= $GAME['Turn']; $turn++) {
+      if ($turn == ($GAME['Turn'] - 5)) echo "</div><div class=InLine>";
+      if (file_exists("Turns/$GAMEID/$turn/CouldC$Fid.html")) echo " <a href=WhatCanIC.php?C=C&Turn=$turn>$turn</a>";
+    }
+    echo "</h2>";
+    if (file_exists("Turns/$GAMEID/" . ($GAME['Turn']-1) . "/CouldM$Fid.html")) {
+      echo "<br>";
+      echo "<h2>After Movement on Turn =&gt;";
+      for($turn=1; $turn <= $GAME['Turn']; $turn++) {
+        if ($turn == ($GAME['Turn'] - 5)) echo "</div><div class=InLine>";
+        if (file_exists("Turns/$GAMEID/$turn/CouldM$Fid.html")) echo " <a href=WhatCanIC.php?C=M&Turn=$turn>$turn</a>";
+      }
+
+      echo "</h2>";
     }
   } else {
     echo "End of Older Turns =&gt; <div id=ExpandTurnsDots class=InLine><b onclick=ExpandTurns()>...</b></div><div id=HiddenTurns hidden>";
     for($turn=1; $turn <= $GAME['Turn']; $turn++) {
       if ($turn == ($GAME['Turn'] - 5)) echo "</div><div class=InLine>";
       if (file_exists("Turns/$GAMEID/$turn/CouldC$Fid.html")) echo " <a href=WhatCanIC.php?C=C&Turn=$turn>$turn</a>";
-    }    
+    }
     echo "</div></h2>";
     if (file_exists("Turns/$GAMEID/" . ($GAME['Turn']-1) . "/CouldM$Fid.html")) {
       echo "<br>";
@@ -46,9 +58,9 @@
       for($turn=1; $turn <= $GAME['Turn']; $turn++) {
         if ($turn == ($GAME['Turn'] - 5)) echo "</div><div class=InLine>";
         if (file_exists("Turns/$GAMEID/$turn/CouldM$Fid.html")) echo " <a href=WhatCanIC.php?C=M&Turn=$turn>$turn</a>";
-      }  
-   
-      echo "</div></h2>";    
+      }
+
+      echo "</div></h2>";
     }
   }
   echo "</div>";
@@ -56,7 +68,7 @@
  //     if (preg_match('/<div class=FullD hidden>/',$txt,$mtch)) {
 
  //     }
-    
+
   if (isset($_REQUEST['Turn'])) {
     $Turn = $_REQUEST['Turn'];
     $When = (($File == 'CouldC')? " at the end of " : " After Movement on ");
@@ -65,11 +77,11 @@
     $html = file_get_contents("Turns/$GAMEID/$Turn/$File$Fid.html");
     $html = preg_replace('/Orders: .*?</',"<",$html);
     echo $html;
-  } else {  
+  } else {
     echo "<button class='floatright FullD' onclick=\"($('.FullD').toggle())\">Show Remains of Things</button>";
     echo "<h1>What Can I See Now?</h1>";
     echo WhatCanBeSeenBy($Fid);
   }
-  
-  dotail();  
+
+  dotail();
 ?>
